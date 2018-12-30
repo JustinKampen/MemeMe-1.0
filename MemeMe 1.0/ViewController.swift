a7d6d83a7544e34b9a94f9e8f9c89a7ea8e1c767
Justin <justinkampen@hotmail.com>
2018-12-29 22:32:21 -0800

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Justin Kampen on 12/29/18.
+//  Copyright © 2018 Justin Kampen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
